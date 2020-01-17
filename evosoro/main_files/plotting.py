c32cbdb8780ef433c3de1875c2e56965938a8c57
ZarHenry96 <enrico.zardini@studenti.unitn.it>
2020-01-17 18:02:15 +0100

    bug fix

--- evosoro/main_files/plotting.py
+++ evosoro/main_files/plotting.py
@@ -1,11 +1,11 @@
+import os
+import sys
+sys.path.append(os.getcwd() + "/../..")
 from evosoro.tools.data_analysis import get_fitness_trends, plot_fitness_trends
 
 # directory names
-materials_1 = "./materials_evolution_data/allIndividualsData"
-materials_2 = "./minimize_materials_evolution_data/allIndividualsData"
+materials_1 = "./basic_evolution_pareto_data/allIndividualsData"
 
 mat1_fitness_df = get_fitness_trends(materials_1)
-#mat2_fitness_df = get_fitness_trends(materials_2)
 
 plot_fitness_trends(mat1_fitness_df, "materials_evolution_fitness_trends")
-#plot_fitness_trends(mat2_fitness_df, "minimize_materials_evolution_fitness_trends")
