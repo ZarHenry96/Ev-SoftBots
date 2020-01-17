6a1e742f97faf0ce7576b68928efb5d21a522020
ZarHenry96 <enrico.zardini@studenti.unitn.it>
2020-01-17 16:25:03 +0100

    add parser for bestOfGen.txt files

---
+++ evosoro/main_files/bestComparison.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import sys
+
+if len(sys.argv) == 1:
+    print("Missing parameters! Usage: python experimentName,label ...")
+    exit(1)
+
+for i in range(1, len(sys.argv)):
+    input = sys.argv[i].split(",")
+    exp_data = pd.read_csv(input[0]+"/bestSoFar/bestOfGen.txt", sep="\t\t", engine='python')
+    plt.plot(exp_data["fitness"], label=input[1])
+
+plt.legend()
+
+plt.xlabel("Generation")
+plt.ylabel("Best fitness")
+
+plt.show()
