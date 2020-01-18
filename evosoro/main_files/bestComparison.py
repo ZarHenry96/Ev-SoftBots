12b6bbe68f2bfb7412c302ac016fbcf40118dc03
ZarHenry96 <enrico.zardini@studenti.unitn.it>
2020-01-18 18:30:08 +0100

    add plotting script for best individuals

--- evosoro/main_files/bestComparison.py
+++ evosoro/main_files/bestComparison.py
@@ -6,10 +6,14 @@ if len(sys.argv) == 1:
     print("Missing parameters! Usage: python experimentName,label ...")
     exit(1)
 
+figure = plt.figure()
+name = ""
+
 for i in range(1, len(sys.argv)):
     input = sys.argv[i].split(",")
     exp_data = pd.read_csv(input[0]+"/bestSoFar/bestOfGen.txt", sep="\t\t", engine='python')
     plt.plot(exp_data["fitness"], label=input[1])
+    name += ("-"+input[0])
 
 plt.legend()
 
@@ -17,3 +21,5 @@ plt.xlabel("Generation")
 plt.ylabel("Best fitness")
 
 plt.show()
+
+figure.savefig("comparison"+name+".pdf")
